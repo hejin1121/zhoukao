039d7c05dfe121f84de0af3377451f96b7f06f4c
hj <hj2229778947@163.com>
2019-03-18 08:58:08 +0800

    周考

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const minCss = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const webserver = require('gulp-webserver');
+gulp.task('devScss', () => {
+    return gulp.src('')
+})
